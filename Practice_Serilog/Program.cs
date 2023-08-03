c633ee068005982adef2be2a974ea75dc3d6eac1
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2023-08-03 12:13:28 +0200

    init

---
+++ Practice_Serilog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Serilog;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console() // pretty print logs to the console
+            .WriteTo.File(new Serilog.Formatting.Compact.CompactJsonFormatter(), "./logs/log.txt") // log objects to a file
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Starting up");
+
+            // logs a structured event
+            var position = new { Latitude = 25.0, Longitude = 134.0 };
+            var elapsedMs = 34;
+
+            Log.Information("Processed {@Position} in {Elapsed:000} ms.", position, elapsedMs);
+
+            Log.Information("Shutting down");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application start-up failed");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Serilog Demo
+
+A small demo project using Serilog for structured logging.
